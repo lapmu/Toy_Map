caef542e26f8d4a3d0ea7ad27b92f0dd28d7416b
lapmu <antjdgkdl@gmail.com>
2023-06-05 11:41:51 +0900

    [feat] 기본 지도 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react';
+import styled from 'styled-components';
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+    const options = {
+      //지도를 생성할 때 필요한 기본 옵션
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      level: 3, //지도의 레벨(확대, 축소 정도)
+    };
+
+    const map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MapWapper id='map' />
     </>
-  )
+  );
 }
 
-export default App
+const MapWapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+export default App;
